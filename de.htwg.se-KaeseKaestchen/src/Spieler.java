6554a8e4ffe85d39adf276b79d562cd301803e4f
Matthias Weis <matthias.weis@htwg-konstanz.de>
2014-03-26 12:04:26 +0100

    added Spieler class

---
+++ de.htwg.se-KaeseKaestchen/src/Spieler.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+public class Spieler {
+	
+	Color farbe;
+	String name;
+	int punkte;
+	
+	public Spieler(String pName, Color pFarbe) {
+		farbe = pFarbe;
+		name = pName;
+		
+	}
+	
+	public Spieler(String pName){
+		name = pName;
+		//TODO zufällige farbe
+		
+	}
+}
