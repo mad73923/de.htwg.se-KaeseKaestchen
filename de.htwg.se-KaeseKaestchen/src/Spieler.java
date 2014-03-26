003011a87edf3aec72f8ece2a0e9e2a02190f1e6
Matthias Weis <matthias.weis@htwg-konstanz.de>
2014-03-26 12:45:34 +0100

    added getter methodes in Spieler class

--- de.htwg.se-KaeseKaestchen/src/Spieler.java
+++ de.htwg.se-KaeseKaestchen/src/Spieler.java
@@ -9,12 +9,26 @@ public class Spieler {
 	public Spieler(String pName, Color pFarbe) {
 		farbe = pFarbe;
 		name = pName;
+		punkte = 0;
 		
 	}
 	
 	public Spieler(String pName){
 		name = pName;
+		punkte = 0;
 		//TODO zufällige farbe
 		
+	}
+
+	public Color getFarbe() {
+		return farbe;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPunkte() {
+		return punkte;
 	}
 }
