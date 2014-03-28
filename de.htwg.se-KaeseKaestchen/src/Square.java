30e23e00706e174424537a7495ad9ad370deaaf6
Matthias Weis <matthias.weis@htwg-konstanz.de>
2014-03-28 18:03:37 +0100

    prepared for unroot

--- de.htwg.se-KaeseKaestchen/src/Square.java
+++ de.htwg.se-KaeseKaestchen/src/Square.java
@@ -4,12 +4,8 @@ public class Square {
 	Player owner;
 	Square[] neighbors;
 	
-	public Square(Square neighborTop, Square neighborBottom, Square neighborLeft, Square neighborRight) {
-		neighbors = new Square[4];
-		neighbors[0] = neighborTop;
-		neighbors[1] = neighborBottom;
-		neighbors[2] = neighborLeft;
-		neighbors[3] = neighborRight;
+	public Square() {
+
 		
 	}
 
