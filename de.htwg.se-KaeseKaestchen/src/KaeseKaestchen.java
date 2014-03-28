90e16fc76fc8feed6dced396bb13a7eaabbfc7e1
Blinderaufkoks <klein.winz@web.de>
2014-03-28 12:46:19 +0100

    content added to constructor 'Playfield'

--- de.htwg.se-KaeseKaestchen/src/KaeseKaestchen.java
+++ de.htwg.se-KaeseKaestchen/src/KaeseKaestchen.java
@@ -12,7 +12,7 @@ public class KaeseKaestchen {
 	UI theUI;
 	
 	
-	
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
--- de.htwg.se-KaeseKaestchen/src/PlayField.java
+++ de.htwg.se-KaeseKaestchen/src/PlayField.java
@@ -2,10 +2,11 @@
 public class PlayField {
 	
 	KaeseKaestchen theControl;
-	
+	Square matrix[][];
 	
 	public PlayField(KaeseKaestchen pTheControl, int sizeX, int sizeY) {
 		theControl = pTheControl;
+		matrix = new Square[sizeX][sizeY];
 	}
 
 }
