12b880dc820f18e1465ab20204b12da0ff773301
matweis <matweis@3DLAB12.in.fhkn.de>
2014-04-11 18:26:08 +0200

    Point test implementiert

---
+++ de.htwg.se-KaeseKaestchen/test/de/htwg/se/KaeseKaestchen/tests/PointTest.java
@@ -0,0 +1,43 @@
+package de.htwg.se.KaeseKaestchen.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.htwg.se.KaeseKaestchen.model.Point;
+
+public class PointTest {
+	
+	Point thePoint;
+
+	@Before
+	public void setUp() throws Exception {
+		thePoint = new Point(100, 200);
+	}
+
+	@Test
+	public void testGetValX() {
+		assertEquals(100, thePoint.getValX());
+	}
+
+	@Test
+	public void testSetValX() {
+		assertEquals(100, thePoint.getValX());
+		thePoint.setValX(55);
+		assertEquals(55, thePoint.getValX());
+	}
+
+	@Test
+	public void testGetValY() {
+		assertEquals(200, thePoint.getValY());
+	}
+
+	@Test
+	public void testSetValY() {
+		assertEquals(200, thePoint.getValY());
+		thePoint.setValY(255);
+		assertEquals(255, thePoint.getValY());
+	}
+
+}
