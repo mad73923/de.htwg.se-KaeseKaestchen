b27abc09486c772e8f427d6fb4cc54699ed0f5b2
Blinderaufkoks <klein.winz@web.de>
2014-05-24 14:30:51 +0200

    handled InputMismatchException

--- de.htwg.se-KaeseKaestchen/src/de/htwg/se/kaesekaestchen/ui/TUI.java
+++ de.htwg.se-KaeseKaestchen/src/de/htwg/se/kaesekaestchen/ui/TUI.java
@@ -3,6 +3,7 @@ package de.htwg.se.kaesekaestchen.ui;
 import de.htwg.se.kaesekaestchen.controller.KaeseKaestchenControl;
 
 import java.awt.Color;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TUI extends UI {
@@ -18,37 +19,66 @@ public class TUI extends UI {
 
 	@Override
 	protected void refreshUI() {
-		// TODO Auto-generated method stub
+		
+		
 	}
 
 	@Override
 	public void showWelcomeMessage() {
-		//TODO collect new game data
-		System.out.println("Welcome to KaeseKaestchen!\n(c) Matthias Weis & Joey Rieg\nHTWG Konstanz SS 2014\n\n");
-		System.out.printf("Enter number of Players (max. %d Players):\n", colors.length);
-		
-		int numberOfPlayers = in.nextInt();
-		String playerNames[] = new String[numberOfPlayers];
-		for(int i = 0; i < numberOfPlayers; i++){
-			System.out.printf("Name of Player%d\n", i+1);
-			playerNames[i] = in.next();
+
+		try {
+			//TODO collect new game data
+			System.out.println("Welcome to KaeseKaestchen!\n(c) Matthias Weis & Joey Rieg\nHTWG Konstanz SS 2014\n\n");
+			System.out.printf("Enter number of Players (max. %d Players):\n", colors.length);
+			
+			int numberOfPlayers = in.nextInt();
+			String playerNames[] = new String[numberOfPlayers];
+			
+			
+			for(int i = 0; i < numberOfPlayers; i++){
+				System.out.printf("Name of Player%d\n", i+1);
+				playerNames[i] = in.next();
+			}
+			
+			
+			System.out.println("\nEnter size of playfield:");
+			System.out.print("x: ");
+			sizeX = in.nextInt();
+			System.out.print("Y: ");
+			sizeY = in.nextInt();
+			if(sizeX <= 2 || sizeY <= 2){
+				// throw Exception
+			}
+			
+			
+			System.out.printf("Playfield size is x: %d\ty: %d\n\n", sizeX, sizeY);
+			System.out.println("Let's play!");
+			
+			
+			
+			controller.startNewGame(playerNames, colors, sizeX+1, sizeY+1);
+			
+			refreshUI();
+			// UPDATE UI --> only Playfield with Players and Points
+			System.out.println(controller.getPlayField());
+			
+			for(int i = 0; i < playerNames.length; i++) {
+				//Where to get points of players???
+				System.out.printf("%s: x Points\t", playerNames[i]);
+				if(i == 3) {
+					System.out.println();
+				}
+			}
+				
+			
+		} catch (Exception e) {
+			if (e instanceof InputMismatchException) {
+				System.out.println("Please type a number!");
+				
+			}
+			
 		}
-		System.out.println("\nEnter size of playfield:");
-		//TODO x and y > 2
-		System.out.print("x: ");
-		sizeX = in.nextInt();
-		System.out.print("Y: ");
-		sizeY = in.nextInt();
-		System.out.printf("Playfield size is x: %d\ty: %d\n\n", sizeX, sizeY);
-		System.out.println("Let's play!");
-		
-		controller.startNewGame(playerNames, colors, sizeX, sizeX);
-		System.out.println(controller.getPlayField());
-		
-		for(int i = 0; i < playerNames.length; i++) {
-			System.out.printf("%s: x Points\t", playerNames[i]);
-		}
-		
+
 	}
 
 	@Override
